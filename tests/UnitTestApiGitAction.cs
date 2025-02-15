bd5e0d6fa1a80ab9faa3bed8df7593583a8f2dfa
Welington Marquezini Valverde Da Silva <welington_marquezini88@live.com>
2025-02-14 22:32:43 -0300

    GitAction

---
+++ tests/UnitTestApiGitAction.cs
@@ -0,0 +1,36 @@
+﻿namespace ApiGitAction.Tests;
+
+public class UnitTestApiGitAction
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(1, 1);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        Assert.Equal(1, 1);
+    }
+    [Fact]
+    public void Test3()
+    {
+        Assert.Equal(1, 1);
+    }
+    [Fact]
+    public void Test4()
+    {
+        Assert.Equal(1, 1);
+    }
+    [Fact]
+    public void Test5()
+    {
+        Assert.Equal(1, 2);
+    }
+    [Fact(Skip = "Ainda não implementado")]
+    public void Test6()
+    {
+        Assert.Equal(1, 1);
+    }
+}
